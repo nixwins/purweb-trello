be4d7d54bcb547a750385526bdd69d0dd21028ef
nixwins <halaulilau@gmail.com>
2022-01-03 18:45:43 +0600

    Added cross env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+  ],
 })
 export class AppModule {}
