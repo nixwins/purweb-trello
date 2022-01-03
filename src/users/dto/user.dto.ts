cfd06ce5377b9b86263b1463d200a42936c14d76
nixwins <halaulilau@gmail.com>
2022-01-03 23:25:51 +0600

    removed password

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -6,7 +6,4 @@ export class UserDto {
 
   @IsEmail({}, { message: 'Некоректный email' })
   readonly email: string;
-
-  @MinLength(6, { message: 'Пароль не может быть меньше 6' })
-  readonly password: string;
 }
